ece77624d2d247e1bc4d1837cd0ecef44049ee5e
Jorge Lobo <jorge-lobo@outlook.pt>
2024-07-24 19:26:51 +0100

    feat(user-data): Add `formatNumberWithThousandSeparator` method for formatting numerical values with a thousand separator

--- app/src/main/java/com/example/inquizitive/ui/home/userData/UserDataViewModel.kt
+++ app/src/main/java/com/example/inquizitive/ui/home/userData/UserDataViewModel.kt
@@ -11,6 +11,9 @@ import com.example.inquizitive.data.user.UserRepository
 import com.example.inquizitive.ui.common.BaseViewModel
 import com.example.inquizitive.utils.AppConstants
 import kotlinx.coroutines.launch
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
+import java.util.Locale
 
 class UserDataViewModel(application: Application) : BaseViewModel(application), LifecycleObserver {
 
@@ -20,18 +23,18 @@ class UserDataViewModel(application: Application) : BaseViewModel(application), 
 
     private val _loggedInUser = MutableLiveData<User?>()
     private val _username = MutableLiveData<String>()
-    private val _userCoins = MutableLiveData<Int>()
-    private val _userQuizzes = MutableLiveData<Int>()
-    private val _userPoints = MutableLiveData<Int>()
-    private val _userBest = MutableLiveData<Int>()
+    private val _userCoins = MutableLiveData<String>()
+    private val _userQuizzes = MutableLiveData<String>()
+    private val _userPoints = MutableLiveData<String>()
+    private val _userBest = MutableLiveData<String>()
     private val _userAvatar = MutableLiveData<String>()
     private val _welcomeMessage = MutableLiveData<String>()
 
     val username: LiveData<String> get() = _username
-    val userCoins: LiveData<Int> get() = _userCoins
-    val userQuizzes: LiveData<Int> get() = _userQuizzes
-    val userPoints: LiveData<Int> get() = _userPoints
-    val userBest: LiveData<Int> get() = _userBest
+    val userCoins: LiveData<String> get() = _userCoins
+    val userQuizzes: LiveData<String> get() = _userQuizzes
+    val userPoints: LiveData<String> get() = _userPoints
+    val userBest: LiveData<String> get() = _userBest
     val userAvatar: LiveData<String> get() = _userAvatar
     val welcomeMessage: LiveData<String> get() = _welcomeMessage
 
@@ -65,12 +68,19 @@ class UserDataViewModel(application: Application) : BaseViewModel(application), 
 
     private fun updateUserLiveData(user: User) {
         _username.value = user.username ?: ""
-        _userCoins.value = user.actualCoins ?: 0
-        _userQuizzes.value = user.quizzesPlayed ?: 0
-        _userPoints.value = user.totalPoints ?: 0
-        _userBest.value = user.bestResult ?: 0
+        _userCoins.value = formatNumberWithThousandSeparator(user.actualCoins ?: 0)
+        _userQuizzes.value = formatNumberWithThousandSeparator(user.quizzesPlayed ?: 0)
+        _userPoints.value = formatNumberWithThousandSeparator(user.totalPoints ?: 0)
+        _userBest.value = formatNumberWithThousandSeparator(user.bestResult ?: 0)
         _userAvatar.value = user.avatar ?: ""
         _welcomeMessage.value = "Hello ${user.username ?: "User"} !"
+    }
+
+    private fun formatNumberWithThousandSeparator(number: Int): String {
+        val symbols = DecimalFormatSymbols(Locale.getDefault()).apply {
+            groupingSeparator = ' '
+        }
+        return DecimalFormat("#,###", symbols).format(number)
     }
 
     override fun onError(message: String?, validationErrors: Map<String, ArrayList<String>>?) {
